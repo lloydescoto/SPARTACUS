7ac2fbdb625922b77dcda52f3aa1fe632712d6b9
Lloyd Escoto <42109272+lloydescoto22@users.noreply.github.com>
2018-10-15 14:10:07 +0800

    Finalize System

--- home.php
+++ home.php
@@ -490,7 +490,7 @@
 	<link rel="icon" type="image/png" href="dumbbell.png">
 	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="exercise/font/flaticon.css"> 
 	<script src="http://maps.googleapis.com/maps/api/js"></script>
 	<script src="main.js"></script>
